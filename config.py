bc006cfa9dd4c366fc1559ba9926c6581e12e98e
𝙉𝙖𝙮𝙧𝙖 ✗ 𝙍𝙤𝙗𝙤𝙩 <168265371+NayraXRobot@users.noreply.github.com>
2024-08-01 12:54:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,77 +4,69 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
+API_ID = int(getenv("API_ID", 22871746))
+API_HASH = getenv("API_HASH", "1221eabe731f81b4ec603548ccf57224")
+OWNER_ID = int(getenv("OWNER_ID", "6065525851"))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "AboutshayarAkhawab")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "ll_ZIDDI_SHAYAR_ll")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "SHAYARAKHAWAB")
 
 # Random Start Images
 IMG = [
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
+    "https://telegra.ph/file/ca413e9bb4060f631544d.jpg",
 ]
 
 
 # Random Stickers
 STICKER = [
-    "",
-    "",
-    "",
+     "CAACAgUAAx0CYlaJawABBy4vZaieO6T-Ayg3mD-JP-f0yxJngIkAAv0JAALVS_FWQY7kbQSaI-geBA",
+    "CAACAgUAAx0CYlaJawABBy4rZaid77Tf70SV_CfjmbMgdJyVD8sAApwLAALGXCFXmCx8ZC5nlfQeBA",
+    "CAACAgUAAx0CYlaJawABBy4jZaidvIXNPYnpAjNnKgzaHmh3cvoAAiwIAAIda2lVNdNI2QABHuVVHgQ",
 ]
 
 
 EMOJIOS = [
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
+     "💞", "🦋", "🔍", "🧪", "🦋", "⚡️", "🔥", "🦋", "🎩", "🌈", "🍷", "🥂", "🦋", "🥃", "🥤", "🕊️",
+    "🦋", "🦋", "🕊️", "🦋", "🕊️", "🦋", "🦋", "🦋", "🪄", "💌", "🦋", "🦋", "🧨"
 ]
